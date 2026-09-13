f915def2bd102877af8764b39b183453b34920ec
harsharora-code <harshpunyani20@gmail.com>
2026-09-13 13:13:48 +0530

    remove db access from worker to remove vulnerability

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { createClient } from "redis";
 import { prisma } from "./db";
+import { captureRejectionSymbol } from "node:events";
 const client = createClient();
+const resultSubcriber = createClient();
+await client.connect();
+await resultSubcriber.connect();
 
-client.connect();
+const RESULTS_CHANNEL = "submission_results";
 
 const app = express();
 app.use(express.json());
@@ -23,11 +27,24 @@ app.post('/submission', async (req, res) => {
         }
     })  
     
-    client.lPush("problems", JSON.stringify({submissionId: response.id, userId, problemId, code, language}));
+   await client.lPush("problems", JSON.stringify({submissionId: response.id, userId, problemId, code, language}));
     res.json({
         message: "pending",
         id: response.id
     })
+})
+
+await resultSubcriber.subscribe(RESULTS_CHANNEL,  async(message) => { 
+    try {
+        const {submissionId, status, output} = JSON.parse(message);
+        await prisma.submissions.update({
+            where: {id: submissionId},
+            data: {status, output}
+        });
+        console.log(`Database update for submissionId - ${submissionId}`);
+    }catch(err) {
+        console.error("submssion not update", err);
+    }
 })
 
 app.get("/submission/:submissionId", async(req, res) => {
